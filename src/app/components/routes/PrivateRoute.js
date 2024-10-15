f26f8e99fb96a2e5f3b9e44e2b7a88cc3baddf8e
ParthGondaliya2611 <parthgondaliya5959@gmail.com>
2024-10-15 22:04:16 +0530

    change the auth file path in routes

--- src/app/components/routes/PrivateRoute.js
+++ src/app/components/routes/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import Token from "../../../utils/auth";
+import Token from "../../../utils/auth"
 
 const PrivateRoute = () => {
   const token = Token();
