9306a87c963b90abda5be3df57d7582fada1cfac
ParthGondaliya2611 <parthgondaliya5959@gmail.com>
2024-10-17 17:36:42 +0530

    change the style of the login and the register

--- src/app/components/common/Navbar/index.jsx
+++ src/app/components/common/Navbar/index.jsx
@@ -200,7 +200,7 @@ const Navbar = ({ children }) => {
                           <MenuItem>
                             <NavLink
                               to="/login"
-                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                              className="block px-4 py-2 text-sm text-black font-medium data-[focus]:bg-gray-100 data-[focus]:text-gray-900 "
                             >
                               <h1 className="flex  items-center gap-2">
                                 {" "}
