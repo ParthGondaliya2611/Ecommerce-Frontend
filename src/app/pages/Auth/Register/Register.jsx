03cccd765a742fafa01ecccf93ee43944462073e
ParthGondaliya2611 <parthgondaliya5959@gmail.com>
2024-10-21 09:54:10 +0530

    here the same like remove the answer unused the field

--- src/app/pages/Auth/Register/Register.jsx
+++ src/app/pages/Auth/Register/Register.jsx
@@ -12,7 +12,6 @@ const Register = () => {
     email: "",
     password: "",
     phone: "",
-    answer: "",
   });
 
   const handleinput = (e) => {
@@ -143,25 +142,6 @@ const Register = () => {
                   </div>
                 </div>
 
-                <div>
-                  <label
-                    htmlFor="Answer"
-                    className="block text-start  text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Answer
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      name="answer"
-                      required
-                      type="text"
-                      onChange={handleinput}
-                      placeholder="What Is Your Favourite Sports ?"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-                 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
                 <div>
                   <button
                     type="submit"
